3862bcb1908765f75986f7eca6fd5e3cc29e96c5
Kacem Bouden <kacembouden@gmail.com>
2022-10-07 11:34:58 +0100

    bit

--- run.py
+++ run.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html') 
 
+@app.route('/index.html')
+def index1():
+    return render_template('index.html')
 
 
 @app.route('/about.html')
